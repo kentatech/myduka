ef26840fbae1e43139f43160e2594dc29ae1c6a3
kentatech <logankenta254@gmail.com>
2024-12-09 06:05:37 -0800

    option in form

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request,redirect
 from database import conn, cur
 
-# flask name initiates app
+# flask name initiates app- class obj
 app = Flask(__name__)
 
 # Define a custom filter- this will be used to format the date(should be done after importing datetime)
@@ -36,8 +36,10 @@ def products():
         print(name, buying_price, selling_price, stock_quantity) 
         if selling_price < buying_price:
             return "Selling price should be greater than buying price"
+        
         query="insert into products(name,buying_price,selling_price,stock_quantity) "\
         "values('{}',{},{},{})".format(name,buying_price,selling_price,stock_quantity)
+
         cur.execute(query)
         conn.commit()
         return redirect("/products")
@@ -45,9 +47,23 @@ def products():
         
     
 
-@app.route("/sales")
+@app.route("/sales",methods=["GET", "POST"])
 def salez():
-    cur.execute("select sales.ID, products.Name, sales.quantity, sales.created_at from sales inner join products on sales.pid = products.id")
-    return render_template("sales.html",sales=cur.fetchall(),products=cur.fetchall())
+    if request.method=="POST":
+        pid=request.form["pid"]
+        amount=request.form["amount"]
+        print(pid,amount)
+        query_s="insert into sales(pid,quantity,created_at) "\
+        "values('{}',{},{})".format(pid,amount,'now()')
+        cur.execute(query_s)
+        conn.commit()
+        return redirect("/sales")
+    else:
+        cur.execute("select * from products")
+        products=cur.fetchall()
+        cur.execute("select sales.ID, products.Name, sales.quantity, sales.created_at "\
+                    "from sales inner join products on sales.pid = products.id")
+        sales=cur.fetchall()
+        return render_template("sales.html",products=products,sales=sales)
 
 app.run(debug=True)
